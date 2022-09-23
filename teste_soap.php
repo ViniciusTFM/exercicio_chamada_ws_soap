dc33d04771857fa77fd5d2064d30a6d1e0cd4724
ViniciusTFM <viniciusos.2006@gmail.com>
2022-09-23 18:41:47 -0300

    Update teste_soap.php

--- teste_soap.php
+++ teste_soap.php
@@ -8,18 +8,14 @@
 	// define os parâmetros de entrada 
 	$data = new stdClass();
 
-	$data->intA			= 1;
-	$data->intB 		= 1;
+	$data->intA = 1;
+	$data->intB = 1;
 	
-
 	try {
-
 		$result = $cliente->Add($data);
-		
 		print_r($result);
 	
 	} catch(SoapFault $e ) {
-
 		print_r('<b>Erro: </b>');
 		print_r($e);
 	}
