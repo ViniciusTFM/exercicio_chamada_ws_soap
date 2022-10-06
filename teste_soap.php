c0af047ebda486f7425a28d76458907f8c5f5433
unknown <viniciusos.2006@gmail.com>
2022-10-06 20:45:36 -0300

    Exercicio 1

--- teste_soap.php
+++ teste_soap.php
@@ -1,23 +1,48 @@
 <?php
-	print_r('<pre>');
+print_r('<pre>');
 
-	$caminhoWsdl = 'http://www.dneonline.com/calculator.asmx?wsdl';
-	
-	$cliente = new SoapClient($caminhoWsdl, array('trace' => 1));
-	
-	// define os parâmetros de entrada 
-	$data = new stdClass();
+//$caminhoWsdl = 'http://www.dneonline.com/calculator.asmx?wsdl';
+$caminhoWsdl = 'https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL=1';
 
-	$data->intA = 1;
-	$data->intB = 1;
-	
-	try {
-		$result = $cliente->Add($data);
-		print_r($result);
-	
-	} catch(SoapFault $e ) {
-		print_r('<b>Erro: </b>');
-		print_r($e);
-	}
+$cliente = new SoapClient($caminhoWsdl);
 
-	print_r('</pre>');
+//print_r($cliente);
+
+// define os parâmetros de entrada 
+$addIntegerData = new stdClass();
+
+$addIntegerData->Arg1 = 1;
+$addIntegerData->Arg2 = 11;
+
+$divideIntegerData = new stdClass();
+
+$divideIntegerData->Arg1 = 20;
+$divideIntegerData->Arg2 = 2;
+
+
+$findPersonData = new stdClass();
+$findPersonData->id = 1;
+
+try {
+	print_r("-------------------------------------------");
+	print_r("<br>-- Resultado chamada função 'AddInteger' --<br>");
+	print_r("-------------------------------------------<br><br>");
+
+	print_r($cliente->AddInteger($addIntegerData));
+
+	print_r("<br><br>----------------------------------------------");
+	print_r("<br>-- Resultado chamada função 'DivideInteger' --<br>");
+	print_r("----------------------------------------------<br><br>");
+	print_r($cliente->DivideInteger($divideIntegerData));
+	
+	print_r("<br><br>-------------------------------------------");
+	print_r("<br>-- Resultado chamada função 'FindPerson' --<br>");
+	print_r("-------------------------------------------<br><br>");
+	print_r($cliente->FindPerson($findPersonData));
+	
+} catch (SoapFault $e) {
+	print_r('<b>Erro: </b>');
+	print_r($e);
+}
+
+print_r('</pre>');
